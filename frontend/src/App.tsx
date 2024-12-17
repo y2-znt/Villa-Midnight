3c18c3ab9782ed82b4241af91822130a31f8a042
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-17 17:41:22 +0100

    feat: implement header navigation and theme provider integration
    - Added a new Header component for navigation with responsive design.
    - Updated App component structure to include the Header and ThemeProvider for improved layout and styling.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "./components/providers/themeprovider";
+import Header from "./section/Header";
+
 export default function App() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="theme">
+      <div className="mx-auto max-w-7xl">
+        <Header />
+      </div>
+    </ThemeProvider>
   );
 }
---
+++ frontend/src/section/Header.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import Logo from "../components/Logo";
+import { Button } from "../components/ui/button";
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isBurgerActive, setIsBurgerActive] = useState(false);
+
+  const toggleBurger = () => {
+    setIsBurgerActive(!isBurgerActive);
+  };
+
+  const links = [
+    { name: "ACCUEIL", href: "#home" },
+    { name: "A PROPOS", href: "#about" },
+    { name: "SERVICES", href: "#services" },
+    { name: "FAQ", href: "#faq" },
+  ];
+
+  return (
+    <nav className="relative flex items-center justify-between p-4 shadow-md">
+      <a href="/" className="flex items-center gap-2">
+        <Logo />
+        <div className="text-2xl font-semibold text-foreground leading-tight">
+          VILLA <span className="text-primary">MIDNIGHT</span>
+        </div>
+      </a>
+      <div className="hidden lg:flex space-x-4">
+        {links.map((link) => (
+          <a
+            key={link.name}
+            href={link.href}
+            className="hover:text-primary text-base font-semibold"
+          >
+            {link.name}
+          </a>
+        ))}
+      </div>
+      <div className="flex items-center lg:hidden">
+        <button
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen);
+            toggleBurger();
+          }}
+          className="relative flex h-20 w-20 items-center justify-center"
+          aria-label="Burger menu"
+        >
+          <div className="relative w-full">
+            <div
+              className={`absolute left-1/2 h-[1px] w-[40%] -translate-x-1/2 transform bg-white transition-all duration-500 ${
+                isBurgerActive ? "top-0 rotate-45" : "-top-1.5"
+              }`}
+            ></div>
+            <div
+              className={`absolute left-1/2 h-[1px] w-[40%] -translate-x-1/2 transform bg-white transition-all duration-500 ${
+                isBurgerActive ? "top-0 -rotate-45" : "top-1.5"
+              }`}
+            ></div>
+          </div>
+        </button>
+      </div>
+      <div
+        className={`fixed top-32 right-0 z-50 w-full bg-background text-xl text-foreground transition-transform duration-500 ease-in-out lg:hidden ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <ul className="p-4 space-y-8 flex flex-col items-center">
+          {links.map((link) => (
+            <li key={link.name}>
+              <a
+                href={link.href}
+                onClick={() => {
+                  setIsMenuOpen(!isMenuOpen);
+                  toggleBurger();
+                }}
+                className="block hover:text-primary font-medium"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+          <li>
+            <Button variant="default" size="lg">
+              Jouer dès maintenant
+            </Button>
+          </li>
+        </ul>
+      </div>
+      <Button variant="default" size="lg" className="hidden lg:block">
+        Jouer dès maintenant
+      </Button>
+    </nav>
+  );
+}
