69a838e4cc4bcfa18aad211226a56eb84b5020b9
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-17 14:20:16 +0100

    feat: update frontend assets and configuration
    - Changed the favicon to a new logo and updated the document title to "Villa Midnight".
    - Enhanced Tailwind CSS configuration by adding custom colors for primary, background, and text.
    - Updated TypeScript configuration to enable composite builds and JSON module resolution.
    - Removed unused files including vite.svg and App.css, and added new logo assets.
    - Simplified the App component and improved global styles by importing a Google font.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,3 @@
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold text-green-500 underline">
-      Hello world!
-    </h1>
-  );
+  return <h1 className="text-3xl font-bold">Hello world!</h1>;
 }
