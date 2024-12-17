f3971180921ed1204cdd0a808d14e15a20f6ada4
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-17 15:56:12 +0100

    feat: add shadcn lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </div>
+  );
 }
