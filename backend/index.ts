3841a282abe22c7de11f7c40c17003f8b7de2691
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-18 17:59:08 +0100

    feat: update backend server configuration and enhance routing
    - Added CORS middleware to handle cross-origin requests.
    - Refactored API routes to include a '/api' prefix for better organization.
    - Introduced express.json() middleware for parsing JSON request bodies.
    - Improved server startup logging for clarity.

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,25 @@
 // TODO: create express server
 
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes';
-import enigmaRoutes from './routes/enigmaRoutes';
-import { errorHandler } from './middlewares/errorHandler';
+import dotenv from "dotenv";
+import express from "express";
+import corsMiddleware from "./middlewares/corsMiddleware";
+import { errorHandler } from "./middlewares/errorHandler";
+import enigmaRoutes from "./routes/enigmaRoutes";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.use('/users', userRoutes);
-app.use('/enigmas', enigmaRoutes);
+app.use(express.json());
+app.use(corsMiddleware);
+
+app.use("/api/users", userRoutes);
+app.use("/api/enigmas", enigmaRoutes);
 
 app.use(errorHandler);
-  
+
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
+  console.log(`Server is running at http://localhost:${port}`);
+});
