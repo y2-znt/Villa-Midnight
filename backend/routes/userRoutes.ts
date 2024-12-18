4ab48a77d435f773c11b3f1eb2f14953c8e60ad0
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-18 17:58:49 +0100

    refactor: simplify user routes by removing middleware and streamlining route handlers
    - Consolidated user route definitions by directly linking HTTP methods to controller functions.
    - Removed middleware for validation and error handling, enhancing code readability and maintainability.

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,62 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from "express";
 import {
+  createUser,
+  deleteUser,
   getAllUsers,
   getUserById,
-  createUser,
   updateUser,
-  deleteUser,
-} from '../controllers/userController';
-import { validateId } from '../middlewares/validateId';
-import { validateBody } from '../middlewares/validateBody';
+} from "../controllers/userController";
 
 const router = express.Router();
 
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await getAllUsers(req, res);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/:id', validateId('id'), async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await getUserById(req, res);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post(
-  '/',
-  validateBody(['username', 'email', 'password']),
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await createUser(req, res);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-router.put(
-  '/:id',
-  [validateId('id'), validateBody(['username', 'email'])],
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await updateUser(req, res);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-router.delete('/:id', validateId('id'), async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await deleteUser(req, res);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
