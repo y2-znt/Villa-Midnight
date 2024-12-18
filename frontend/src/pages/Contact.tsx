9aaccac24288b953b3dc2a3e5d5a09990f0c8514
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-18 09:37:41 +0100

    feat: add new pages for All Enigmas, Contact, and FAQ
    - Introduced three new pages: All Enigmas, Contact, and FAQ, each featuring a Title component for consistent UI.
    - Each page includes a section with a highlighted title, enhancing the overall user experience and navigation.

---
+++ frontend/src/pages/Contact.tsx
@@ -0,0 +1,9 @@
+import Title from "../components/ui/title";
+
+export default function Contact() {
+  return (
+    <div className="mt-28">
+      <Title text="REJOIGNEZ" highlight="NOUS" />
+    </div>
+  );
+}
