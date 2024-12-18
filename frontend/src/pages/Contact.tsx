7f2b4705c7286a595f7b78a01f48c067033cae6e
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-18 10:16:23 +0100

    feat: update layout and enhance Contact page with dynamic contact list
    - Adjusted margin-top for All Enigmas and FAQ pages for consistent spacing.
    - Enhanced Contact page by adding a dynamic contact list with icons, titles, and descriptions for better user engagement.
    - Improved layout of the Contact page to be more visually appealing and organized.

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,9 +1,51 @@
+import { Calendar, LucideIcon, Mail, MapPin, Phone } from "lucide-react";
 import Title from "../components/ui/title";
 
+type ContactTypes = {
+  icon: LucideIcon;
+  title: string;
+  description: string;
+};
+
 export default function Contact() {
+  const contactList: ContactTypes[] = [
+    {
+      icon: Calendar,
+      title: "7 jours / 7",
+      description: "De 10h à 00h",
+    },
+    {
+      icon: Phone,
+      title: "Téléphone",
+      description: "02 99 99 99 99",
+    },
+    {
+      icon: MapPin,
+      title: "5 Rue des Écrivains",
+      description: "44000 Nantes",
+    },
+    {
+      icon: Mail,
+      title: "Email",
+      description: "villamidnight@gmail.com",
+    },
+  ];
+
   return (
-    <div className="mt-28">
+    <div className="mt-10 md:mt-28 flex flex-col items-center">
       <Title text="REJOIGNEZ" highlight="NOUS" />
+      <div className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-4">
+        {contactList.map((contact) => (
+          <div className="flex items-center gap-4 transition-transform transform hover:scale-105">
+            <contact.icon className="text-primary size-10" />
+
+            <div className="flex flex-col gap-x-4 text-xl">
+              <p>{contact.title}</p>
+              <p>{contact.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- frontend/src/pages/FAQ.tsx
+++ frontend/src/pages/FAQ.tsx
@@ -2,7 +2,7 @@ import Title from "../components/ui/title";
 
 export default function FAQ() {
   return (
-    <div className="mt-28">
+    <div className="mt-10 md:mt-28">
       <Title text="FOIRE AUX" highlight="QUESTIONS" />
     </div>
   );
