daef9634529092c30abe5ca8d0255ad15f76c1fa
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-18 18:13:14 +0100

    feat: implement user service with CRUD operations
    - Added functions to fetch all users and fetch a user by ID.
    - Implemented user creation, modification, and deletion functionalities.
    - Enhanced error handling for each operation to provide clearer feedback on failures.

---
+++ backend/services/userService.ts
@@ -0,0 +1,71 @@
+import prisma from "../src/prisma/prismaClient";
+
+export const fetchAllUsers = async () => {
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } catch (error) {
+    throw new Error(`Failed to fetch users: ${(error as Error).message}`);
+  }
+};
+
+export const fetchUserById = async (id: string) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user;
+  } catch (error) {
+    throw new Error(`Failed to fetch user: ${(error as Error).message}`);
+  }
+};
+
+export const addUser = async ({
+  username,
+  email,
+  password,
+}: {
+  username: string;
+  email: string;
+  password: string;
+}) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
+    return user;
+  } catch (error) {
+    throw new Error(`Failed to create user: ${(error as Error).message}`);
+  }
+};
+
+export const modifyUser = async (
+  id: string,
+  { username, email }: { username?: string; email?: string }
+) => {
+  try {
+    const user = await prisma.user.update({
+      where: { id },
+      data: {
+        username,
+        email,
+      },
+    });
+    return user;
+  } catch (error) {
+    throw new Error(`Failed to update user: ${(error as Error).message}`);
+  }
+};
+
+export const removeUser = async (id: string) => {
+  try {
+    await prisma.user.delete({ where: { id } });
+  } catch (error) {
+    throw new Error(`Failed to delete user: ${(error as Error).message}`);
+  }
+};
