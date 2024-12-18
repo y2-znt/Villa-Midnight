c344391e6afe8ec15078c6e1f0f705cfd339513e
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-18 17:57:45 +0100

    feat: add CORS middleware and refactor error handler
    - Introduced a new CORS middleware to handle cross-origin requests, allowing specific origins and managing preflight requests.
    - Refactored the error handler for improved readability by restructuring the function parameters.

--- backend/middlewares/errorHandler.ts
+++ backend/middlewares/errorHandler.ts
@@ -1,6 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from "express";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack); // Log de l'erreur pour le développeur
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err.stack);
   res.status(500).json({ message: err.message });
 };
