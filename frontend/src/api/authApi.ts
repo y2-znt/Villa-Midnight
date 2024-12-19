6451aa4196823511ed4c5844d2d7b1beb8cbcd2a
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-19 22:40:54 +0100

    feat(frontend): update dependencies and add authentication API
    - Added new dependencies: @hookform/resolvers, react-hook-form, and zod for form handling and validation.
    - Introduced authApi module for user registration functionality with error handling.
    - Updated Input component styling for improved UI consistency.
    - Excluded js-big-decimal from dependency optimization in Vite configuration.

---
+++ frontend/src/api/authApi.ts
@@ -0,0 +1,20 @@
+const API_URL = "http://localhost:4000/api/auth";
+export const registerUser = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  const response = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
+  });
+
+  if (response.ok) {
+    console.log("Inscription réussie");
+  } else {
+    console.error("Erreur lors de l'inscription");
+  }
+};
