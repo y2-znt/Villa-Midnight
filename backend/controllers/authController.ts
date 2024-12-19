2f50cc7ffa7b25d5965cbb03365df7e731b2dcdf
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-19 17:45:52 +0100

    feat(backend): add authentication controller with user registration, login, and session management
    - Implemented authController to handle user registration, login, and retrieval of the current user.
    - Added error handling for validation errors using Zod and improved response messages for various scenarios.
    - Integrated cookie management for storing authentication tokens securely during login and logout processes.
    - Enhanced user experience by providing clear feedback on authentication actions.

---
+++ backend/controllers/authController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { z } from "zod";
+import { loginUser, registerUser } from "../services/authService";
+import { AuthenticatedRequest } from "../utils/express";
+
+const handleErrorResponse = (res: Response, error: unknown) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).json({ message: "Validation error", errors: error.errors });
+  } else {
+    res.status(400).json({ message: (error as Error).message });
+  }
+};
+
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = await registerUser(req.body);
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    handleErrorResponse(res, error);
+  }
+};
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { user, token } = await loginUser(req.body);
+
+    console.log("token", token);
+
+    res.cookie("authToken", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict", // Helps prevent CSRF attacks
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    res.status(200).json({ message: "Login successful", user, token });
+  } catch (error) {
+    handleErrorResponse(res, error);
+  }
+};
+
+export const getCurrentUser = async (
+  req: AuthenticatedRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    if (!req.user) {
+      res.status(401).json({ message: "Unauthorized: No user found" });
+    }
+
+    const user = req.user;
+    res.status(200).json({ message: "User retrieved successfully", user });
+  } catch (error) {
+    console.error("Error retrieving current user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = async (req: Request, res: Response): Promise<void> => {
+  try {
+    res.clearCookie("authToken");
+    console.log("token", req.cookies.authToken);
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred during logout" });
+  }
+};
