a78659c12fe6f64bd3765bcb99b5a6f2d53759f5
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-19 22:55:46 +0100

    feat(frontend): update user registration API to include password confirmation
    - Modified the registerUser function to accept a confirmPassword parameter for enhanced validation.
    - Updated the request body to include confirmPassword when registering a new user, ensuring password confirmation is handled correctly.

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,15 +1,17 @@
 const API_URL = "http://localhost:4000/api/auth";
+
 export const registerUser = async (
   username: string,
   email: string,
-  password: string
+  password: string,
+  confirmPassword: string
 ) => {
   const response = await fetch(`${API_URL}/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, email, password }),
+    body: JSON.stringify({ username, email, password, confirmPassword }),
   });
 
   if (response.ok) {
