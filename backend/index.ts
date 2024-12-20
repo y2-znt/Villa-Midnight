39353f736503e8538666813fc1c038de87c4898c
basile <basilezic@gmail.com>
2024-12-20 09:10:19 +0100

    Feat Swagger

--- backend/index.ts
+++ backend/index.ts
@@ -6,6 +6,9 @@ import { errorHandler } from "./middlewares/errorHandler";
 import authRoutes from "./routes/authRoutes";
 import enigmaRoutes from "./routes/enigmaRoutes";
 import userRoutes from "./routes/userRoutes";
+import * as bodyParser from "body-parser";
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "./swagger_output.json";
 
 dotenv.config();
 
@@ -16,12 +19,16 @@ app.use(express.json());
 app.use(corsMiddleware);
 app.use(cookieParser());
 app.use(errorHandler);
+app.use(bodyParser.json());
 
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/enigmas", enigmaRoutes);
 app.use("/api/auth", authRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+export const routes = express.Router();
