30f89f47806e8f2b660903e0a1355a9a8f8fee83
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-21 12:39:33 +0100

    feat(enigma): implement EnigmaCard component and integrate enigma fetching
    - Added EnigmaCard component to display individual enigma details, including title, description, difficulty, image, number of participants, and hours required.
    - Updated AllEnigmas page to fetch and display a list of enigmas using the new fetchAllEnigmas function.
    - Introduced Enigma type definition to ensure type safety for enigma objects throughout the application.

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,11 @@
+export type Enigma = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  difficulty: number;
+  numberOfParticipants: number;
+  numberOfHours: number;
+  createdAt: string;
+  updatedAt: string;
+};
