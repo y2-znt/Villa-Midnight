4df06232fdaefd99a92e6562753d64fc059c176c
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-21 12:19:34 +0100

    feat(enigmaApi): add fetchAllEnigmas function for API data retrieval
    - Introduced a new function to fetch all enigmas from the API, handling errors and logging responses.
    - This addition enhances the frontend's capability to retrieve and manage enigma data effectively.

---
+++ frontend/src/api/enigmaApi.ts
@@ -0,0 +1,19 @@
+const API_URL = "http://localhost:4000/api/enigmas";
+
+export const fetchAllEnigmas = async () => {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(
+        errorData.message || "Erreur lors de la récupération des énigmes"
+      );
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des énigmes:", error);
+    throw error;
+  }
+};
