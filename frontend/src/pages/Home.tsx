ab93bb119f962cdac6238ddac3a73445544469f9
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-22 21:04:38 +0100

    feat(frontend): enhance Home component layout with FAQ and Contact sections
    - Added FAQ and Contact components to the Home page, improving user access to important information.
    - Refactored background styling for better visual consistency and layout organization.
    - Ensured proper spacing and alignment for a more polished user experience.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,29 +1,33 @@
 import Particles from "../components/ui/particles";
 import Hero from "../sections/Hero";
+import Contact from "./Contact";
+import FAQ from "./FAQ";
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <div
-          className="absolute inset-0 bg-cover bg-center -z-10"
-          style={{
-            backgroundImage: `url('/assets/hero_img.jpg')`,
-            opacity: 0.3,
-          }}
-        />
-        <Particles
-          className="absolute inset-0 hidden xl:flex -z-10"
-          quantity={200}
-          ease={80}
-          color={"#ffffff"}
-          size={1}
-          refresh
-        />
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-background -z-10" />
-      </div>
-      <div>
-        <Hero />
+      <div
+        className="absolute inset-0 bg-cover bg-center -z-10"
+        style={{
+          backgroundImage: `url('/assets/hero_img.jpg')`,
+          opacity: 0.3,
+        }}
+      />
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-background -z-10" />
+      <Particles
+        className="absolute inset-0 hidden xl:flex -z-10"
+        quantity={200}
+        ease={80}
+        color={"#ffffff"}
+        size={1}
+        refresh
+      />
+
+      <Hero />
+      <div className="mt-[50vh]"></div>
+      <FAQ />
+      <div className="my-[30vh]">
+        <Contact />
       </div>
     </div>
   );
