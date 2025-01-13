19111bca455fcc530f460a888b2c1ddadd30d47f
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-13 09:53:42 +0100

    feat(frontend): add user deletion functionality in Profile page
    - Implemented deleteUser API function to allow users to delete their accounts.
    - Enhanced Profile component with a confirmation dialog and a button for account deletion.
    - Updated user context state and navigation upon successful account deletion, improving user experience.

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -9,3 +9,13 @@ export const fetchUserById = async (id: string) => {
   const data = await response.json();
   return data;
 };
+
+export const deleteUser = async (id: string) => {
+  const response = await fetch(`${API_BASE_URL}/users/${id}`, {
+    method: "DELETE",
+  });
+  if (!response.ok) {
+    throw new Error("Erreur lors de la suppression de l'utilisateur");
+  }
+  return response.json();
+};
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,17 +1,43 @@
+import { useNavigate } from "react-router";
+import { deleteUser } from "../api/userApi";
+import { Button } from "../components/ui/button";
 import Title from "../components/ui/title";
 import { useAuthContext } from "../context/AuthContext";
 
 export default function Profile() {
-  const { authUser } = useAuthContext();
+  const { authUser, setAuthUser } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleDeleteUser = async () => {
+    const userId = authUser?.user?.id;
+    if (!userId) {
+      console.error("User is not authenticated.");
+      return;
+    }
+
+    const confirmDelete = window.confirm(
+      "Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible."
+    );
+    if (confirmDelete) {
+      await deleteUser(userId);
+      setAuthUser(null);
+      navigate("/");
+    }
+  };
 
   return (
     <div className="p-4">
       <Title text="MON" highlight="PROFIL" />
       <div className="mt-4">
-        <p className="text-lg font-semibold">
+        <p className="text-lg font-medium">
           Nom d'utilisateur: {authUser?.user.username}
         </p>
-        <p className="text-lg font-semibold">Email: {authUser?.user.email}</p>
+        <p className="text-lg font-medium">Email: {authUser?.user.email}</p>
+      </div>
+      <div className="mt-4">
+        <Button onClick={handleDeleteUser} variant="destructive">
+          Supprimer mon compte
+        </Button>
       </div>
     </div>
   );
