2d65641ddf58064a15c523176a277ee0f59a73e2
basile <basilezic@gmail.com>
2025-01-14 16:12:59 +0100

    Fix: Remove oauth package

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,6 @@ import * as bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
-import { auth, requiredScopes } from "express-oauth2-jwt-bearer";
 import swaggerUi from "swagger-ui-express";
 import corsMiddleware from "./middlewares/corsMiddleware";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -22,37 +21,6 @@ app.use(cookieParser());
 app.use(errorHandler);
 app.use(bodyParser.json());
 
-// Authorization middleware. When used, the Access Token must
-// exist and be verified against the Auth0 JSON Web Key Set.
-const checkJwt = auth({
-  audience: process.env.AUDIENCE,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
-});
-
-// This route doesn't need authentication
-app.get("/api/public", function (req, res) {
-  res.json({
-    message:
-      "Hello from a public endpoint! You don't need to be authenticated to see this.",
-  });
-});
-
-// This route needs authentication
-app.get("/api/private", checkJwt, function (req, res) {
-  res.json({
-    message:
-      "Hello from a private endpoint! You need to be authenticated to see this.",
-  });
-});
-
-const checkScopes = requiredScopes("read:messages");
-
-app.get("/api/private-scoped", checkJwt, checkScopes, function (req, res) {
-  res.json({
-    message:
-      "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
-  });
-});
 
 // Routes
 app.use("/api/users", userRoutes);
