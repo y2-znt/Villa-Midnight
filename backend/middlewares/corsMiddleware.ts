962fcba3515424d3891ce83c1ee92d55e4f8c9df
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-15 11:14:12 +0100

    fix: enhance CORS middleware to support additional headers and methods

--- backend/middlewares/corsMiddleware.ts
+++ backend/middlewares/corsMiddleware.ts
@@ -9,18 +9,23 @@ const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
   if (origin && allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
   }
-
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   // Handle preflight requests
   if (req.method === "OPTIONS") {
-    res.status(204).end();
-  } else {
-    next();
+    return res.status(204).end();
   }
+
+  next();
 };
 
 export default corsMiddleware;
