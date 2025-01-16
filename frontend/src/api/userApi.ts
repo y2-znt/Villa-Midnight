b1aa8f55bf55baee081cbf96a953ef067796eff9
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-16 16:09:54 +0100

    fix: improve error handling in fetchUserById function to log error details and provide clearer error messages

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -2,13 +2,19 @@ import { API_BASE_URL } from "../config/apiClient";
 import { UserUpdateType } from "../types/types";
 
 export const fetchUserById = async (id: string) => {
-  const response = await fetch(`${API_BASE_URL}/users/${id}`);
-  if (!response.ok) {
-    throw new Error("Erreur lors de la récupération de l'utilisateur");
+  try {
+    const response = await fetch(`${API_BASE_URL}/users/${id}`);
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Error response text:", errorText);
+      throw new Error("Erreur lors de la récupération de l'utilisateur");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Erreur lors de la récupération de l'utilisateur:", error);
+    throw error;
   }
-
-  const data = await response.json();
-  return data;
 };
 
 export const updateUser = async (id: string, user: UserUpdateType) => {
