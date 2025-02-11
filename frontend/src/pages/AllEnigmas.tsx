0f7e0224a7e84a93287c88437c5cbfdd2ef845e5
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-02-11 06:54:23 +0100

    feat: replace API call with fakeAdminEnigmas data for initial rendering

--- frontend/src/pages/AllEnigmas.tsx
+++ frontend/src/pages/AllEnigmas.tsx
@@ -1,35 +1,20 @@
-import { useEffect, useState } from "react";
-import { deleteEnigma, fetchAllEnigmas } from "../api/enigmaApi";
+import { deleteEnigma } from "../api/enigmaApi";
 import EnigmaCard from "../components/shared/EnigmaCard";
 import Title from "../components/ui/title";
+import { fakeAdminEnigmas } from "../data/data";
 import { EnigmaType } from "../types/types";
 
 export default function AllEnigmas() {
-  const [enigmas, setEnigmas] = useState<EnigmaType[]>([]);
-
-  useEffect(() => {
-    const getEnigmas = async () => {
-      try {
-        const data = await fetchAllEnigmas();
-        setEnigmas(data.slice(0, 3));
-      } catch (error) {
-        console.error("Erreur lors de la récupération des énigmes:", error);
-      }
-    };
-
-    getEnigmas();
-  }, []);
-
   return (
     <div>
       <Title text="DÉFIS DE LA" highlight="VILLA" />
       <div className="mx-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-2 mt-16">
-        {enigmas.length === 0 ? (
+        {fakeAdminEnigmas.length === 0 ? (
           <div className="text-center text-2xl font-bold">
             Aucune énigme disponible.
           </div>
         ) : (
-          enigmas.map((enigma: EnigmaType) => (
+          fakeAdminEnigmas.map((enigma: EnigmaType) => (
             <EnigmaCard
               key={enigma.id}
               enigma={enigma}
