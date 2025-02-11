981df8ce6e123302ec8b5c6de93462e96d37d4c9
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-02-11 22:32:31 +0100

    feat: update enigma schema to use enum for difficulty and add Radix UI select component

--- backend/dist/schemas/enigmaSchema.js
+++ backend/dist/schemas/enigmaSchema.js
@@ -5,7 +5,7 @@ const zod_1 = require("zod");
 exports.enigmaSchema = zod_1.z.object({
     title: zod_1.z.string().min(1, "Title is required"),
     description: zod_1.z.string().min(1, "Description is required"),
-    difficulty: zod_1.z.number().min(1, "Difficulty must be at least 1"),
+    difficulty: zod_1.z.enum(["ONE", "TWO", "THREE"]),
     image: zod_1.z.string().url("Image must be a valid URL"),
     userId: zod_1.z.string().min(1, "User ID is required"),
     numberOfParticipants: zod_1.z
