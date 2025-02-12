4f13fd0b7c2e1730c5db6d0513752d723bed1311
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-02-12 19:56:13 +0100

    fix: update DifficultyIndicator to accept dynamic difficulty values

--- frontend/src/components/shared/DifficultyIndicator.tsx
+++ frontend/src/components/shared/DifficultyIndicator.tsx
@@ -1,7 +1,7 @@
 import Logo from "./Logo";
 
 type DifficultyIndicatorProps = {
-  difficulty: "ONE" | "TWO" | "THREE";
+  difficulty: string;
 };
 
 const DifficultyIndicator = ({ difficulty }: DifficultyIndicatorProps) => {
@@ -9,14 +9,14 @@ const DifficultyIndicator = ({ difficulty }: DifficultyIndicatorProps) => {
     ONE: 1,
     TWO: 2,
     THREE: 3,
-  };
+  }[difficulty];
 
   return (
     <div className="flex flex-wrap items-center gap-2">
       <span className="font-bold text-primary italic text-xl md:text-3xl">
         DIFFICULTÉ
       </span>{" "}
-      {[...Array(difficultyLevels[difficulty])].map((_, index) => (
+      {[...Array(difficultyLevels)].map((_, index) => (
         <Logo key={index} />
       ))}
     </div>
