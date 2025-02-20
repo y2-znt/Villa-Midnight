25673be44223be0cf60c74d765370f0808044b2c
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-02-20 20:56:51 +0100

    refactor: centralize error handling across controllers using new errorHandler utility

--- backend/dist/controllers/userController.js
+++ backend/dist/controllers/userController.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUser = exports.updateUser = exports.createUser = exports.getEnigmaByUserId = exports.getUserById = exports.getAllUsers = void 0;
 const userService_1 = require("../services/userService");
+const errorHandler_1 = require("../utils/errorHandler");
 const getAllUsers = async (req, res) => {
     try {
         const users = await (0, userService_1.fetchAllUsers)();
         res.status(200).json(users);
     }
     catch (error) {
-        res.status(500).json({ message: error.message });
+        (0, errorHandler_1.handleErrorResponse)(res, error);
     }
 };
 exports.getAllUsers = getAllUsers;
@@ -18,7 +19,7 @@ const getUserById = async (req, res) => {
         res.status(200).json(user);
     }
     catch (error) {
-        res.status(500).json({ message: error.message });
+        (0, errorHandler_1.handleErrorResponse)(res, error);
     }
 };
 exports.getUserById = getUserById;
@@ -28,42 +29,37 @@ const getEnigmaByUserId = async (req, res) => {
         res.status(200).json(enigmas);
     }
     catch (error) {
-        res.status(500).json({ message: error.message });
+        (0, errorHandler_1.handleErrorResponse)(res, error);
     }
 };
 exports.getEnigmaByUserId = getEnigmaByUserId;
 const createUser = async (req, res) => {
-    const { username, email, password } = req.body;
     try {
-        const user = await (0, userService_1.addUser)({ username, email, password });
+        const user = await (0, userService_1.addUser)(req.body);
         res.status(201).json(user);
     }
     catch (error) {
-        res.status(500).json({ message: error.message });
+        (0, errorHandler_1.handleErrorResponse)(res, error);
     }
 };
 exports.createUser = createUser;
 const updateUser = async (req, res) => {
-    const { username, email } = req.body;
     try {
-        const user = await (0, userService_1.modifyUser)(req.params.id, {
-            username,
-            email,
-        });
+        const user = await (0, userService_1.modifyUser)(req.params.id, req.body);
         res.status(200).json(user);
     }
     catch (error) {
-        res.status(500).json({ message: error.message });
+        (0, errorHandler_1.handleErrorResponse)(res, error);
     }
 };
 exports.updateUser = updateUser;
 const deleteUser = async (req, res) => {
     try {
         await (0, userService_1.removeUser)(req.params.id);
-        res.status(200).json({ message: "User deleted" });
+        res.status(204).json({ message: "User deleted successfully" });
     }
     catch (error) {
-        res.status(500).json({ message: error.message });
+        (0, errorHandler_1.handleErrorResponse)(res, error);
     }
 };
 exports.deleteUser = deleteUser;
