19832a33771a6995c906af67601173d4c0cf71db
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-02-23 14:47:30 +0100

    feat: Implement Cloudinary image management for user profile

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import cloudinary from "../config/cloudinaryConfig";
 import {
   addUser,
   fetchAllUsers,
@@ -84,7 +85,6 @@ export const createUser = async (
     handleErrorResponse(res, error);
   }
 };
-
 export const updateUser = async (
   req: AuthenticatedRequest,
   res: Response
@@ -101,8 +101,32 @@ export const updateUser = async (
       });
       return;
     }
-    const { username, email } = req.body;
-    const user = await modifyUser(req.params.id, { username, email });
+
+    // Get the current user to check if they have an existing avatar
+    const currentUser = await fetchUserById(req.params.id);
+    let imageUrl = req.body.avatarUrl;
+
+    if (req.file) {
+      // Delete the old image if it exists
+      if (currentUser.avatarUrl) {
+        try {
+          const publicId = `villa-midnight/${
+            currentUser.avatarUrl.split("/").pop()?.split(".")[0]
+          }`;
+          await cloudinary.uploader.destroy(publicId);
+          console.log(`Old user image deleted from Cloudinary: ${publicId}`);
+        } catch (error) {
+          console.error("Error deleting old image from Cloudinary:", error);
+        }
+      }
+      // Set the new image URL
+      imageUrl = req.file.path;
+    }
+
+    const user = await modifyUser(req.params.id, {
+      ...req.body,
+      avatarUrl: imageUrl,
+    });
     res.status(200).json(user);
   } catch (error) {
     handleErrorResponse(res, error);
@@ -125,7 +149,22 @@ export const deleteUser = async (
       });
       return;
     }
+
+    // Fetch the user to get the image URL
+    const userToDelete = await fetchUserById(req.params.id);
+
+    // Delete the user
     await removeUser(req.params.id);
+
+    // Delete the user's image from Cloudinary if it exists
+    if (userToDelete.avatarUrl) {
+      const publicId = `villa-midnight/${
+        userToDelete.avatarUrl.split("/").pop()?.split(".")[0]
+      }`;
+      await cloudinary.uploader.destroy(publicId);
+      console.log(`User image deleted from Cloudinary: ${publicId}`);
+    }
+
     res.status(204).json({ message: "User deleted successfully" });
   } catch (error) {
     handleErrorResponse(res, error);
