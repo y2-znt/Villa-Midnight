8e0c99c0be5df28eea6579a57297877856ab1582
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-03-04 21:55:04 +0100

    feat: Modify enigma retrieval to allow public access and include creator details

--- backend/routes/enigmaRoutes.ts
+++ backend/routes/enigmaRoutes.ts
@@ -12,7 +12,7 @@ import upload from "../middlewares/upload";
 const router = express.Router();
 
 router.get("/", authMiddleware, getAllEnigmas);
-router.get("/:id", authMiddleware, getEnigmaById);
+router.get("/:id", getEnigmaById);
 router.post("/", authMiddleware, upload.single("image"), createEnigma);
 router.put("/:id", authMiddleware, upload.single("image"), updateEnigma);
 router.delete("/:id", authMiddleware, deleteEnigma);
