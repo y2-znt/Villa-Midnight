9cb2e65e6afb08a33c456669d57a00770f7fbb30
y2_znt <yonideserbaix@gmail.com>
2025-03-09 00:07:59 +0100

    refactor: Remove unused metadata generation for EnigmaPage

--- frontend/app/(app)/enigma/[id]/page.tsx
+++ frontend/app/(app)/enigma/[id]/page.tsx
@@ -1,28 +1,5 @@
-import { fakeAdminEnigmas } from "@/data/data";
-import { fetchEnigmaById } from "@/lib/api/enigmaApi";
 import EnigmaDetails from "../EnigmaDetails";
 
 export default function EnigmaPage() {
   return <EnigmaDetails />;
-}
-
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  let enigma = null;
-  let fakeEnigma = null;
-
-  try {
-    enigma = await fetchEnigmaById(params.id);
-  } catch (error) {
-    console.error(error);
-  }
-
-  fakeEnigma = fakeAdminEnigmas.find((e) => e.id === params.id);
-
-  const selectedEnigma = enigma || fakeEnigma;
-
-  return {
-    title: `${selectedEnigma.title} - La Villa Midnight`,
-    description: selectedEnigma.description,
-    images: selectedEnigma.image,
-  };
 }
